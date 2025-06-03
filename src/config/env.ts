9efd9d05d5dff3abb8f28a7d719ef3f363240144
Joss Bleach <joss.bleach@outlook.com>
2025-06-03 16:26:28 +0100

    setup better auth

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,3 @@
-// src/env.mjs
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-core/presets-zod";
 import { z } from "zod";
@@ -6,10 +5,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().url().min(1),
   },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
   },
   extends: [vercel()],
 });
